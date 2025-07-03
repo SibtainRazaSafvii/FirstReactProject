2fb876d61ccfaf7f19562041d81d0a86e5b6a897
Sibtain <sibtainsafviraza@gmail.com>
2025-07-03 16:57:21 +0530

    added assets

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,15 @@ function App() {
   
 
   return (
-    <>
-     <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <main>
+      <div className="pattern">
+        <div>
+          <header>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+        </div>
+      </div>
+    </main>
   )
 }
 
