c7053395c524ff79b84077dd09dafd9183d13edc
Sibtain <sibtainsafviraza@gmail.com>
2025-07-06 10:06:37 +0530

    setup backend

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import './App.css'
 import Search from './components/search'
 import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
-
+import { useDebounce } from 'react-use'
+import { updateSearchCount } from './appwrite'
+const Api= 'https://api.themoviedb.org/3'
 const url = 'https://api.themoviedb.org/3/discover/movie?&language=en-US&sort_by=popularity.desc';
 const options = {
   method: 'GET',
@@ -18,12 +20,22 @@ function App() {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList,setmovieList] = useState([]);
   const [isLoading,setisLoading] = useState(false);
+  const [debouncedSearchTerm, setDebounceSearchTerm] = useState("")
+ 
+  useDebounce( () => setDebounceSearchTerm(searchTerm),500,[searchTerm])
 
-  const fetchMovies = async () => {
+
+  const fetchMovies = async (query = "") => {
     setisLoading(true)
     setErrorMessage("");
+
     try {
-      const response = await fetch(url, options);
+      const endpoint =query 
+      ? `${Api}/search/movie?query=${encodeURIComponent(query)}`
+      : `${Api}/discover/movie?sort_by=popularity.desc`;
+
+
+      const response = await fetch(endpoint, options);
       if (!response.ok) {
         throw new Error("Failed to fetch movies");
       }
@@ -36,6 +48,11 @@ function App() {
       }
       setmovieList(data.results || [])
 
+      if(query && data.results.length >0 ){
+        await updateSearchCount(query,data.results[0])
+      } 
+    
+      
 
     } catch (error) {
       console.error("Error catching Movies", error);
@@ -47,8 +64,8 @@ function App() {
 
   // ✅ useEffect must be inside App()
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
 
   return (
     <main>
@@ -61,25 +78,24 @@ function App() {
               Without the Hassle
             </h1>
             <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-<h1 className="text-white">{searchTerm}</h1>
 </header>
 
 <section className="all-movies">
   <h2 className='mt-[40px]'>All Movies</h2>
   {isLoading ? (
-      <Spinner />
+                <Spinner />
 
-  ) : errorMessage ? (
-    <p className="text-red-500">{errorMessage}</p>
-  ) : (
-    <ul>
-      {movieList.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
+              ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+              ) : (
+              <ul>
+                {movieList.map((movie) => (
+                 <MovieCard key={movie.id} movie={movie} />
 
         
-      ))}
-    </ul>
-  )}
+                  ))}
+              </ul>
+              )}
 </section>
         </div>
       </div>
@@ -88,3 +104,4 @@ function App() {
 }
 
 export default App;
+// Input debouncing is used to Optimize Search 
